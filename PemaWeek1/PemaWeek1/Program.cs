907ba5bc6642d239aa2c77b5d4e4db2fc4a29764
dukpapp <162934259+dukpapp@users.noreply.github.com>
2024-03-21 22:54:41 -0400

    Fixing my project

---
+++ PemaWeek1/PemaWeek1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PemaWeek1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name: ");
+
+            //Read user input
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, World! This is {name}");
+
+            Console.WriteLine("Please Enter Your Age: ");
+
+            //Read user Input
+
+            string age = Console.ReadLine();
+
+            int ageInNumber = Convert.ToInt32(age);
+
+            Console.WriteLine($"I am {ageInNumber}");
+
+            Console.WriteLine($"Next year I will be {ageInNumber + 1}");
+
+        }
+    }
+}
